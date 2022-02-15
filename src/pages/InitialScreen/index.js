bc1e6ccbbe6bb125d0a46860f0cb7a3ca6d49425
clarice42 <claramaia820@gmail.com>
2022-02-15 19:08:30 -0300

    create animal register screen and add navigation

--- src/pages/InitialScreen/index.js
+++ src/pages/InitialScreen/index.js
@@ -1,22 +1,36 @@
-import React from "react"
-import {View, Text, Image} from "react-native"
+import React from "react";
+import { View, Text, Image } from "react-native";
 import FlatButton from "../../components/InitialScreen/button";
-import styles from "./styles"
+import styles from "./styles";
 
 export const InitialScreen = (props) => {
-    return(
-        <View style={styles.container}>
-           <Text style={styles.title}>Olá!</Text>
-           <Text style={styles.subTittle}>Bem vindo ao Meau!{"\n"} Aqui você pode adotar, doar e ajudar {"\n"} cães e gatos com facilidade.{"\n"} Qual seu interesse?</Text>
-            <FlatButton text='ADOTAR'/>
-            <FlatButton text='AJUDAR'/>
-            <FlatButton text='CADASTRAR ANIMAL'/>
-            <FlatButton onPress = {()=> props.navigation.navigate('UserRegister')} text='CADASTRO PESSOAL'/>
-           <Text style={styles.login} onPress = {()=> props.navigation.navigate('Login')} >login</Text>
-           <Image
-                source={require('../../../assets/Meau_marca_2.png')}
-                style={styles.logo}
-           />
-        </View>
-    );
-}
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Olá!</Text>
+      <Text style={styles.subTittle}>
+        Bem vindo ao Meau!{"\n"} Aqui você pode adotar, doar e ajudar {"\n"}{" "}
+        cães e gatos com facilidade.{"\n"} Qual seu interesse?
+      </Text>
+      <FlatButton text="ADOTAR" />
+      <FlatButton text="AJUDAR" />
+      <FlatButton
+        onPress={() => props.navigation.navigate("AnimalRegister")}
+        text="CADASTRAR ANIMAL"
+      />
+      <FlatButton
+        onPress={() => props.navigation.navigate("UserRegister")}
+        text="CADASTRO PESSOAL"
+      />
+      <Text
+        style={styles.login}
+        onPress={() => props.navigation.navigate("Login")}
+      >
+        login
+      </Text>
+      <Image
+        source={require("../../../assets/Meau_marca_2.png")}
+        style={styles.logo}
+      />
+    </View>
+  );
+};
